97237e67fdd63901967c47138bac144383ddbbb8
Jeff-Adler <Jeffm.adler@gmail.com>
2021-09-11 16:33:23 -0400

    implemented user entity validations

--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -1,15 +1,19 @@
-import { Entity, PrimaryGeneratedColumn, Column, UpdateDateColumn, CreateDateColumn } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, UpdateDateColumn, CreateDateColumn, Unique } from 'typeorm';
 import { IUser } from '@interfaces/user.interface';
+import { IsEmail, MinLength } from 'class-validator';
 
 @Entity()
+@Unique(['email'])
 export class User implements IUser {
   @PrimaryGeneratedColumn()
   id!: number;
 
   @Column()
+  @IsEmail()
   email!: string;
 
   @Column()
+  @MinLength(8)
   password!: string;
 
   @Column()
