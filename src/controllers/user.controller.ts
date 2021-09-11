39fdebc2b80e1a52853505b26324f9de5efd6b32
Jeff-Adler <Jeffm.adler@gmail.com>
2021-09-11 14:09:43 -0400

    adjusted logger middleware to show full error stack"

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,12 +1,14 @@
+import { Logger } from '@/utils/logger';
 import { UserService } from '@services/user.service';
 import { NextFunction, Request, Response } from 'express';
 
 export class UserController {
   private readonly userService: UserService = new UserService();
 
-  getUsers(req: Request, res: Response, next: NextFunction) {
+  async getUsers(req: Request, res: Response, next: NextFunction) {
     try {
-      const users = this.userService.findUsers();
+      const users = await this.userService.findUsers();
+      Logger.info('users');
 
       return res.status(200).send(users);
     } catch (error) {
--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -6,9 +6,10 @@ const errorMiddleware = (error: HttpException, req: Request, res: Response, next
   try {
     const status: number = error.status || 500;
     const message: string = error.message || 'Something went wrong';
+    const stack: string | undefined = error.stack;
 
-    Logger.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`);
-    res.status(status).json({ message });
+    Logger.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}\n Stack:: ${stack}`);
+    res.status(status).json({ message, stack });
   } catch (error) {
     next(error);
   }
