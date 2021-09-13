2e123248b2bb27204e173805dff2a40edd0afd8f
Jeff-Adler <Jeffm.adler@gmail.com>
2021-09-13 16:06:18 -0400

    built viable seed method

--- src/database/migration/1631562818169-seeds.ts
+++ src/database/migration/1631562818169-seeds.ts
@@ -3,7 +3,6 @@ import { getConnection, MigrationInterface, QueryRunner } from 'typeorm';
 
 export class seeds1631562818169 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    console.log('hit seed file');
     const dbConnection = getConnection('development');
     // Get all entities
     const entities = dbConnection.entityMetadatas;
@@ -14,13 +13,26 @@ export class seeds1631562818169 implements MigrationInterface {
       await repository.clear();
     }
 
+    // Seed test user
+    await dbConnection.getRepository('user').save({ email: 'test@test.com', password: '12345678' });
+
+    // Seed random users
     for (let i = 0; i < 20; i++) {
       const email = faker.internet.email();
       const password = faker.internet.password(8);
       await dbConnection.getRepository('user').save({ email, password });
-      console.log('hit loop');
     }
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const dbConnection = getConnection('development');
+    // Get all entities
+    const entities = dbConnection.entityMetadatas;
+
+    // Wipe database
+    for (const entity of entities) {
+      const repository = dbConnection.getRepository(entity.name);
+      await repository.clear();
+    }
+  }
 }
