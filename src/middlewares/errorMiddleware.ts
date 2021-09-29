243c3b737df8f620f33b7f5e9fa86044ef278acb
Jeff-Adler <Jeffm.adler@gmail.com>
2021-09-29 15:00:34 -0400

    updated error handling

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -6,10 +6,10 @@ const errorMiddleware = (error: HttpException, req: Request, res: Response, next
   try {
     const status: number = error.status || 500;
     const message: string = error.message || 'Something went wrong';
-    const stack: string | undefined = error.stack;
+    const stack: string = error.stack || 'No stack trace';
 
     Logger.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}\n Stack:: ${stack}`);
-    res.status(status).json({ message, stack });
+    res.status(status).json({ message });
   } catch (error) {
     next(error);
   }
