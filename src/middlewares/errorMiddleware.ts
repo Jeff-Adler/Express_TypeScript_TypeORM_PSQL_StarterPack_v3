e908ef66284d9db1c13ddfab0a5e715ab2c532ce
Jeff-Adler <Jeffm.adler@gmail.com>
2021-10-02 11:31:42 -0400

    added void return types to controller

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import { HttpException } from '@exceptions/HttpException';
 import { Logger } from '@utils/logger';
 
-const errorMiddleware = (error: HttpException, req: Request, res: Response, next: NextFunction) => {
+const errorMiddleware = (error: HttpException, req: Request, res: Response, next: NextFunction): void => {
   try {
     const status: number = error.status || 500;
     const message: string = error.message || 'Something went wrong';
